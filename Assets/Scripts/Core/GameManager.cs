8843000b05a7417ba1d9cba6d2ac4d891714232d
ahmetselahattin <a.selehaddin58@gmail.com>
2026-06-26 00:20:47 +0300

    Phase 1 completed: GameManager added and pieces distributed some mistakes are fixed.

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -4,45 +4,50 @@ public class GameManager : MonoBehaviour
 {
     public Player[] players = new Player[4];
     DeckManager deckManager;
+
     void Start()
     {
-        for (int i = 0; i < 3; i++)
+        //creating 4 player
+        for (int i = 0; i < 4; i++)
         {
             players[i] = new Player();
         }
+        //creating desk
         deckManager = new DeckManager();
         deckManager.CreateDeck();
         deckManager.Shuffle();
         deckManager.DetermineOkey();
         DistributeTiles();
     }
+    //distribution for each player
     public void DistributeTiles()
     {
         for (int i = 0; i < 4; i++)
         {
-            if(i == 0) 
+            //first player get 22 tile
+            if (i == 0)
             {
-                for (int j = 0; j < 22; j++) 
+                for (int j = 0; j < 22; j++)
                 {
-                    players[i].AddTile(deckManager.AllTiles[j]);
-                    deckManager.AllTiles.RemoveAt(j);
+                    //we add tile and remove tile from list for first player
+                    players[i].AddTile(deckManager.AllTiles[0]);
+                    deckManager.AllTiles.RemoveAt(0);
                 }
             }
             else
             {
                 for (int j = 0; j < 21; j++)
                 {
-                    players[i].AddTile(deckManager.AllTiles[j]);
-                    deckManager.AllTiles.RemoveAt(j);
-
+                    //we add tile and remove tile from list for other player
+                    players[i].AddTile(deckManager.AllTiles[0]);
+                    deckManager.AllTiles.RemoveAt(0);
                 }
-
             }
         }
-
     }
+
     void Update()
     {
-        
+
     }
 }
